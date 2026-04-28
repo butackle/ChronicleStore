9b1135fcb7b7db8763c2090096e4a9195a235111
puru <butackle@gmail.com>
2026-04-28 10:28:01 +0900

    add AstronType enum and extend Astron entity with new properties

--- HistoryRecordORM/src/entity/astron.ts
+++ HistoryRecordORM/src/entity/astron.ts
@@ -1,4 +1,5 @@
 import {Column, Entity, PrimaryGeneratedColumn} from 'typeorm'
+import {AstronType} from '../type/astronType';
 
 @Entity()
 export class Astron {
@@ -7,4 +8,27 @@ export class Astron {
 
   @Column()
   name!: string
+
+  // Type of celestial body (e.g., star, planet, moon)
+  @Column({
+    type: 'enum',
+    enum: AstronType,
+    default: null,
+    nullable: true,
+  })
+  type!: AstronType
+
+  // Distance from Earth in light-years
+  @Column('float')
+  distanceLy!: number
+
+  // Datetime when the distance to the celestial body was measured
+  @Column('date')
+  observedAt!: Date
+
+  @Column()
+  createdAt!: Date
+
+  @Column()
+  updatedAt!: Date
 }
---
+++ HistoryRecordORM/src/type/astronType.ts
@@ -0,0 +1,5 @@
+export enum AstronType {
+  'Astron' = 'Astron',
+  'Star' = 'Star',
+  'Planet' = 'Planet',
+}
